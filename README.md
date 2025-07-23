69c2ce26274157677842e76d6d2ee58b72163888
Trithereon <eliottrassa@gmail.com>
2025-07-23 17:09:08 -0400

    Update Readme and trim code for better reusability

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Dropdown module
+Dropdown component
+
+How to use
+
+Add this to your index.js:
+
+```javascript
+import Dropdown from "@trithereon/dropdown/dist/main.js";
+
+document.querySelectorAll(".dropdown").forEach((dropdownContainer) => {
+  new Dropdown(dropdownContainer);
+});
+```
+
+This imports the component and instantiates a Dropdown for each HTML element with class "dropdown".
+
+The structure of the dropdown must follow this template:
+
+```html
+<div class="dropdown">
+  <button data-dropdown="toggle">
+    <svg viewBox="0 0 10 8" width="32">
+      <path
+        d="M1 1h8M1 4h 8M1 7h8"
+        stroke="#000"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  </button>
+  <ul class="dropdown-list">
+    <li class="dropdown-item">
+      <button class="dropdown-button">First Menu Item</button>
+    </li>
+    <li class="dropdown-item">
+      <button class="dropdown-button">Second Menu Item</button>
+    </li>
+    <li class="dropdown-item">
+      <button class="dropdown-button">Third Menu Item</button>
+    </li>
+  </ul>
+</div>
+```
+
+Note: The svg is a hamburger menu icon, but it can be replaced by any image or text instead.
--- src/dropdown.js
+++ src/dropdown.js
@@ -4,56 +4,14 @@
 export default class Dropdown {
   constructor(dropdownContainer) {
     this.dropdownContainer = dropdownContainer;
-    this.toggle = dropdownContainer.querySelector(
-      'button[data-dropdown="toggle"]',
-    );
+    this.toggle = dropdownContainer.querySelector('[data-dropdown="toggle"]');
     this.menu = dropdownContainer.querySelector("ul.dropdown-list");
     this.init();
-    this.dropdownHandlers = {
-      toggle: this.handleToggle,
-      firstItem: this.handleFirstItem,
-      secondItem: this.handleSecondItem,
-      thirdItem: this.handleThirdItem,
-      fourthItem: this.handleFourthItem,
-      fifthItem: this.handleFifthItem,
-      sixthItem: this.handleSixthItem,
-      seventhItem: this.handleSeventhItem,
-      eighthItem: this.handleEighthItem,
-      ninthItem: this.handleNinthItem,
-      tenthItem: this.handleTenthItem,
-    };
   }
-
   init = () => {
-    this.toggle.addEventListener("click", (e) => this.handleClick(e));
-  };
-  handleClick = (e) => {
-    const action = e.target.dataset.dropdown;
-    // Execute the associated function, passing the e onto the next function.
-    if (action) this.dropdownHandlers[action](e);
-    // If the clicked element has no data-dropdown, then ignore the click.
-    else return;
+    this.toggle.addEventListener("click", () => this.handleToggle());
   };
   handleToggle = () => {
     this.menu.classList.toggle("visible");
   };
-  handleFirstItem = () => {};
-
-  handleSecondItem = () => {};
-
-  handleThirdItem = () => {};
-
-  handleFourthItem = () => {};
-
-  handleFifthItem = () => {};
-
-  handleSixthItem = () => {};
-
-  handleSeventhItem = () => {};
-
-  handleEighthItem = () => {};
-
-  handleNinthItem = () => {};
-
-  handleTenthItem = () => {};
 }

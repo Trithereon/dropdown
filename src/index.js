45ac40e75c5088ea14a09f4e99f452f77b43d4bf
Trithereon <eliottrassa@gmail.com>
2025-07-24 02:48:15 -0400

    Remove redundant instantiation from index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import Dropdown from "./dropdown.js";
 import "./styles.css";
 import "./modern-normalize.css";
 
-document.querySelectorAll(".dropdown").forEach((dropdownContainer) => {
-  new Dropdown(dropdownContainer);
-});
-
 export default Dropdown;
